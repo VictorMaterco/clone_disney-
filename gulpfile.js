e4d35a9eaf6c463186ea3c38ecd217fbde64355e
VictorMaterco <vmaterco@gmail.com>
2024-04-17 00:26:31 -0300

    Added : feito o hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
+//instalando plugin do gulp
 const gulp = require('gulp');
+// instalando plugin de Sass
 const sass = require('gulp-sass')(require ('sass'));
+// instalando plugin de minificar imagens
+const imagemin = require('gulp-imagemin');
 
 //criando função para pegar o arquivo de dev  scss e criar o arquvo de programa css.
 function styles() {
@@ -11,8 +15,19 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
+//criando funcao para minificar img
+function images() {
+    //retornando arquivo em que vai ser pego
+    return gulp.src('./src/images/**/*')
+    //comprimindo images
+    .pipe(imagemin())
+    //criando local onde o arquivo vai ficar
+    .pipe(gulp.dest('./dist/images'));
+}
+
 //rodando a função do arquivo SCSS para CSS
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 //fazendo função de vigia automatica
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
